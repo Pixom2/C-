85d1cb65a52d1e6f1b61813489431ed61aea4c42
Przemysław Jordanek <jordanek.przemyslaw@gmail.com>
2018-03-29 19:29:41 +0200

    gotowa I czesc

--- bigProject_interface_s368/bigProject_interface_s368/Form1.Designer.cs
+++ bigProject_interface_s368/bigProject_interface_s368/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.goHere = new System.Windows.Forms.Button();
+            this.goThroughTheDoor = new System.Windows.Forms.Button();
+            this.exits = new System.Windows.Forms.ComboBox();
+            this.description = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // goHere
+            // 
+            this.goHere.Location = new System.Drawing.Point(76, 210);
+            this.goHere.Name = "goHere";
+            this.goHere.Size = new System.Drawing.Size(75, 23);
+            this.goHere.TabIndex = 0;
+            this.goHere.Text = "Idz tutaj:";
+            this.goHere.UseVisualStyleBackColor = true;
+            this.goHere.Click += new System.EventHandler(this.goHere_Click);
+            // 
+            // goThroughTheDoor
+            // 
+            this.goThroughTheDoor.Location = new System.Drawing.Point(76, 249);
+            this.goThroughTheDoor.Name = "goThroughTheDoor";
+            this.goThroughTheDoor.Size = new System.Drawing.Size(295, 23);
+            this.goThroughTheDoor.TabIndex = 1;
+            this.goThroughTheDoor.Text = "Przejdz przez drzwi.";
+            this.goThroughTheDoor.UseVisualStyleBackColor = true;
+            this.goThroughTheDoor.Click += new System.EventHandler(this.goThroughTheDoor_Click);
+            // 
+            // exits
+            // 
+            this.exits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.exits.FormattingEnabled = true;
+            this.exits.Location = new System.Drawing.Point(187, 210);
+            this.exits.Name = "exits";
+            this.exits.Size = new System.Drawing.Size(184, 21);
+            this.exits.TabIndex = 2;
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(76, 45);
+            this.description.Multiline = true;
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(295, 149);
+            this.description.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(449, 305);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.exits);
+            this.Controls.Add(this.goThroughTheDoor);
+            this.Controls.Add(this.goHere);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button goHere;
+        private System.Windows.Forms.Button goThroughTheDoor;
+        private System.Windows.Forms.ComboBox exits;
+        private System.Windows.Forms.TextBox description;
     }
 }
 
--- bigProject_interface_s368/bigProject_interface_s368/Form1.cs
+++ bigProject_interface_s368/bigProject_interface_s368/Form1.cs
@@ -12,85 +12,177 @@ namespace bigProject_interface_s368
 {
     public partial class Form1 : Form
     {
-        public abstract class Location
-        {
-            public Location(string name)
-            {
-                this.Name = name;
-            }
-            public string Name { get; private set; }
-            public Location[] Exits;
+        Location currentLocation;
 
-            public virtual string Description
-            {
-                get
-                {
-                    string description = "Stoisz w: " + Name + ". Widzisz wyjścia do nastepujacych lokalizacji:";
-                    for (int i = 0; i < Exits.Length; i++)
-                    {
-                        description += " " + Exits[i].Name;
-                        if (i != Exits.Length - 1) // -1 bo zaczynam od i=0
-                            description += ", ";
-                    }
-                    description += ".";
-                    return description;
-                }
-            }
-        }
+        RoomWithDoor livingRoom;    // drzwi zewnetrzne
+        Room diningRoom;
+        RoomWithDoor kitchen;
 
-        public class Room : Location
-        {
-            private string decoration;
-            public Room(string name, string decoration) : base(name)
-            {
-                this.decoration = decoration;
-            }
-        }
-
-        public class Outside : Location
-        {
-            private bool hot;
-            public Outside(string name, bool hot) : base(name)
-            {
-                this.hot = hot;
-            }
-            
-            public override string Description
-            {
-                get
-                {
-                    string NewDescription = base.Description;   // TUTAJ CIEKAWE
-                    if (hot)
-                        NewDescription += "Tutaj jest bardzo goraco.";
-                    return NewDescription;
-                }
-            }
-        }
-
-        class OutsideWithDoor : Outside, IHasExteriorDoor
-        {
-                        
-            private string hot { get; }
-
-            string DoorLocation { get; }
-            string DoorDescription { get; set; }
-        }
-
-        class RoomWithDoor : Room, IHasExteriorDoor
-        {
-            string DoorLocation { get; }
-            string DoorDescription { get; set; }
-        }
-
+        OutsideWithDoor frontYard;
+        Outside garden;
+        OutsideWithDoor backYard;
+        
         public Form1()
         {
             InitializeComponent();
+            CreateObject();
+            MoveToANewLocation(livingRoom);
+        }
+
+        private void CreateObject()
+        {
+            livingRoom = new RoomWithDoor("Pokoj dzienny", "antyczny dywan", "debowe drzwi");
+            diningRoom = new Room("jadalnia", "krysztalowy zyrandol");
+            kitchen = new RoomWithDoor("Kuchnia", "stol w kuchni", "plastikowe drzwi");
+            frontYard = new OutsideWithDoor("Podworko przed domem", true, "debowe drzwi");
+            garden = new Outside("Ogrod", false);
+            backYard = new OutsideWithDoor("Podworko z tylu", true, "plastikowe drzwi2");
+
+            diningRoom.Exits = new Location[] { livingRoom, kitchen };
+            livingRoom.Exits = new Location[] { diningRoom };
+            kitchen.Exits = new Location[] { diningRoom };
+            frontYard.Exits = new Location[] { backYard, garden };
+            garden.Exits = new Location[] { frontYard, backYard };
+            backYard.Exits = new Location[] { frontYard, garden };
+
+            livingRoom.DoorLocation = frontYard;
+            kitchen.DoorLocation = backYard;
+
+            frontYard.DoorLocation = livingRoom;
+            backYard.DoorLocation = kitchen; 
+        }
+
+        private void MoveToANewLocation(Location newLocation)
+        {
+            currentLocation = newLocation;
+            exits.Items.Clear();
+
+            for (int i = 0; i < currentLocation.Exits.Length; i++)
+            {
+                exits.Items.Add(currentLocation.Exits[i].Name);
+            }
+            exits.SelectedIndex = 0;
+            description.Text = currentLocation.Description;
+
+            if (currentLocation is IHasExteriorDoor)
+                goThroughTheDoor.Visible = true;
+            else
+                goThroughTheDoor.Visible = false;
+           
+
+        }
+
+        private void goThroughTheDoor_Click(object sender, EventArgs e)
+        {
+            IHasExteriorDoor hasDoor = currentLocation as IHasExteriorDoor; // przeanalizowac
+            MoveToANewLocation(hasDoor.DoorLocation);
+        }
+
+        private void goHere_Click(object sender, EventArgs e)
+        {
+            MoveToANewLocation(currentLocation.Exits[exits.SelectedIndex]);
         }
     }
 
-    internal interface IHasExteriorDoor // internal to public dla innych klas zestawu
+    interface IHasExteriorDoor // internal to public dla innych klas zestawu
     {
-        string DoorLocation { get; }
-        string DoorDescription { get; set; }
+        Location DoorLocation { get; set; }
+        string DoorDescription { get; }
     }
+
+    public abstract class Location
+    {
+        public Location(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; private set; }
+        public Location[] Exits;
+
+        public virtual string Description
+        {
+            get
+            {
+                string description = "Stoisz w: " + Name + ". Widzisz wyjścia do nastepujacych lokalizacji:";
+                for (int i = 0; i < Exits.Length; i++)
+                {
+                    description += " " + Exits[i].Name;
+                    if (i != Exits.Length - 1) // -1 bo zaczynam od i=0
+                        description += ", ";
+                }
+                description += ".";
+                return description;
+            }
+        }
+    }
+
+    public class Room : Location
+    {
+        private string decoration;
+        public Room(string name, string decoration) : base(name)
+        {
+            this.decoration = decoration;
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return base.Description + "Widzisz tutaj " + decoration + ".";
+            }
+        }
+    }
+
+    public class RoomWithDoor : Room, IHasExteriorDoor
+    {
+        public RoomWithDoor(string name, string decoration, string doorDescription) : base(name, decoration)
+        {
+            DoorDescription = doorDescription;
+        }
+        public Location DoorLocation { get; set; }
+        public string DoorDescription { get; private set; }
+    }
+
+
+    public class Outside : Location
+    {
+        private bool hot;
+        public Outside(string name, bool hot) : base(name)
+        {
+            this.hot = hot;
+        }
+
+        public override string Description
+        {
+            get
+            {
+                string NewDescription = base.Description;   // TUTAJ CIEKAWE
+                if (hot)
+                    NewDescription += "Tutaj jest bardzo goraco.";
+                return NewDescription;
+            }
+        }
+    }
+
+    public class OutsideWithDoor : Outside, IHasExteriorDoor
+    {
+        public OutsideWithDoor(string name, bool hot, string doorDescriptions) : base(name, hot)
+        {
+            this.DoorDescription = doorDescriptions;
+        }
+
+        public string DoorDescription { get; private set; }
+
+        public Location DoorLocation { get; set; }
+
+        public override string Description
+        {
+            get
+            {
+                return base.Description + "Widzisz teraz " + DoorDescription + ".";
+            }
+        }
+
+    }
+
 }
