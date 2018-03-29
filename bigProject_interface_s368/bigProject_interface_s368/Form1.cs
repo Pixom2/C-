85f860c1befd9e4c6a2ebc4ae9c017ae60247a6d
Przemysław Jordanek <jordanek.przemyslaw@gmail.com>
2018-03-29 13:41:12 +0200

    test_nr1

---
+++ bigProject_interface_s368/bigProject_interface_s368/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bigProject_interface_s368
+{
+    public partial class Form1 : Form
+    {
+        public abstract class Location
+        {
+            public string Name;
+            public Location[] Exits = new Location[];
+
+        }
+
+        public class Room : Location
+        {
+
+        }
+
+        public class Outside : Location
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
